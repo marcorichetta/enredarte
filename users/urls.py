d5de105f14f033347374b7cd66606164a957ed8d
Marco Richetta <marcorichetta@gmail.com>
2019-07-11 22:05:08 -0300

    Cambios en UI y logica de users
    - Si el user esta logueado se lo redirige al index
    - Creado un layout para users que hereda del base

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+
 from .views import register
 
 urlpatterns = [
 
     path('register/', register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    # If user is logged in, redirect to 'login' url
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html',
+                                                redirect_authenticated_user=True),
+                                                name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),
+                                                  name='logout'),
 
     # Password Reset
     path('password-reset/',
--- users/views.py
+++ users/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib import messages
 from users.forms import UserRegisterForm
 
 # Create your views here.
 
+
 def register(request):
+
+    # Redirect to index
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+
     """ Register a new user """
     if request.method == 'POST':
 
@@ -14,7 +20,8 @@ def register(request):
             form.save()
             # Retrieve username from cleaned_data to show it on message
             username = form.cleaned_data.get('username')
-            messages.success(request, f'{username}, your user has been created. Please, log in!')
+            messages.success(
+                request, f'{username}, tu usuario fue creado. Por favor, inicia sesión')
             return redirect('login')
     # GET Request
     else:
