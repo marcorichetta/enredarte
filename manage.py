41f322a85bc8544f948715c295b8c33bbc00a86d
Marco Richetta <marcorichetta@gmail.com>
2020-05-17 20:36:23 -0300

    Probando TODOs con extension Github

--- manage.py
+++ manage.py
@@ -3,9 +3,9 @@
 import os
 import sys
 
-
+# TODO: #6 Crear config con docker + testing
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enredarte.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "enredarte.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +17,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
