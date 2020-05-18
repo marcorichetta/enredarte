2df2c20ab4d14fd9b51bfdb82d9213b4dc340e43
Marco Richetta <marcorichetta@gmail.com>
2020-05-18 19:33:28 -0300

    Fix

--- gestion/views.py
+++ gestion/views.py
@@ -1,12 +1,6 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import (
-    TemplateView,
-    CreateView,
-    FormView
-)
-from proveedores.models import Proveedor
+from django.views.generic import TemplateView
 from pedidos.models import Pedido
 from clientes.models import Cliente
 from productos.models import Producto
@@ -23,39 +17,31 @@ class Dashboard(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Dashboard, self).get_context_data(**kwargs)
-        context['pedidos'] = Pedido.objects.get_queryset()[:5]
-        context['clientes'] = Cliente.objects.get_queryset()
-        context['productos'] = Producto.objects.get_queryset()[:5]
+        context["pedidos"] = Pedido.objects.get_queryset()[:5]
+        context["clientes"] = Cliente.objects.get_queryset()
+        context["productos"] = Producto.objects.get_queryset()[:5]
         return context
 
 
 def LocalidadCreate(request):
     if request.method == "POST":
 
-        cod_postal = request.POST.get('cod_postal')
-        localidad = request.POST.get('localidad')
-        provincia_id = request.POST.get('provincia_id')
+        cod_postal = request.POST.get("cod_postal")
+        localidad = request.POST.get("localidad")
+        provincia_id = request.POST.get("provincia_id")
 
         # Obtener instancia de Provincia
         provincia = Provincia.objects.get(pk=provincia_id)
 
         # Crear en DB
-        Localidad.objects.create(
-            cod_postal=cod_postal,
-            localidad=localidad,
-            provincia=provincia
-        )
+        Localidad.objects.create(cod_postal=cod_postal, localidad=localidad, provincia=provincia)
 
         # Enviar dict con datos de la nueva Localidad
         nuevaLocalidad = {
-            'cod_postal': cod_postal,
-            'localidad': localidad,
-            'provincia': provincia_id
+            "cod_postal": cod_postal,
+            "localidad": localidad,
+            "provincia": provincia_id,
         }
 
         # Devolver info a JS
         return JsonResponse(nuevaLocalidad)
-
-
-class ViewPrueba(FormView):
-    template_name = 'gestion/prueba_select2.html'
