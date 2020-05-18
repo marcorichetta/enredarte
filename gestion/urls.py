aef191053f43b424c518cbf4819572320e5652ed
Marco Richetta <marcorichetta@gmail.com>
2020-05-18 19:16:31 -0300

    Agregado prospector
    - Limpieza requirements
    - Eliminación prueba select2

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path
-from .views import (
-    Dashboard,
-    ViewPrueba,
-    LocalidadCreate
-)
+from .views import Dashboard, LocalidadCreate
 
-from .forms import FormPrueba
 
 urlpatterns = [
-     path('', Dashboard.as_view(), name='index'),
-
-     # Prueba Select2
-     path('prueba/', ViewPrueba.as_view(form_class=FormPrueba), name='prueba'),
-     # Usada con AJAX en el modalform de creación del cliente
-     path('localidad/new/', LocalidadCreate, name='crearLocalidad')
+    path("", Dashboard.as_view(), name="index"),
+    # Usada con AJAX en el modalform de creación del cliente
+    path("localidad/new/", LocalidadCreate, name="crearLocalidad"),
 ]
