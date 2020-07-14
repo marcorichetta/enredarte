c0d93d325096955d84e027e3e325d02734029146
Marco Richetta <marcorichetta@gmail.com>
2020-07-13 21:12:48 -0300

    Heroku 4

--- enredarte/settings.py
+++ enredarte/settings.py
@@ -159,15 +159,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# BASE_DIR = /home/rich/programming/enredarte
 
 # Whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 
