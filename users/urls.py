5893b6900ddd80a209e99013500dabe87df036a2
Marco Richetta <marcorichetta@gmail.com>
2020-08-12 21:37:25 -0300

    Cambios en app de usuarios
    - Login actualizado
    - Se movieron CSS y se eliminaron algunos templates

--- users/urls.py
+++ users/urls.py
@@ -4,31 +4,18 @@ from django.contrib.auth import views as auth_views
 from .views import register
 
 urlpatterns = [
-
-    path('register/', register, name='register'),
+    path("register/", register, name="register"),
     # If user is logged in, redirect to 'login' url
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html',
-                                                redirect_authenticated_user=True),
-                                                name='login'),
-
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),
-                                                  name='logout'),
-
-    # Password Reset
-    path('password-reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='users/password_reset.html'),
-         name='password_reset'),
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='users/password_reset_done.html'),
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='users/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='users/password_reset_complete.html'),
-         name='password_reset_complete')
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="users/login.html", redirect_authenticated_user=True
+        ),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="users/logout.html"),
+        name="logout",
+    ),
 ]
