4c27dd9634fa1e7e270ab20ccc5e670e80ec9025
Marco Richetta <marcorichetta@gmail.com>
2020-12-10 12:02:12 -0300

    Reporte pedidos por mes con fechas dinámicas

--- reportes/views.py
+++ reportes/views.py
@@ -40,24 +40,38 @@ class PedidosTiempoView(TemplateView):
 
 def pedidos_por_tiempo(request):
 
-    # labels = []
-    data = []
+    str_fecha_inicio = request.GET.get("fecha_inicio") or "2020-6-1"
+    str_fecha_fin = request.GET.get("fecha_fin") or "2020-12-1"
 
-    fecha_inicio = datetime.date(2020, 6, 1)
-    fecha_final = datetime.date(2020, 12, 31)
+    inicio = datetime.datetime.strptime(str_fecha_inicio, "%Y-%m-%d")
+    fin = datetime.datetime.strptime(str_fecha_fin, "%Y-%m-%d")
 
-    jul = Pedido.objects.filter(fecha_pedido__month="7").count()
-    ago = Pedido.objects.filter(fecha_pedido__month="8").count()
-    sept = Pedido.objects.filter(fecha_pedido__month="9").count()
-    octu = Pedido.objects.filter(fecha_pedido__month="10").count()
-    nov = Pedido.objects.filter(fecha_pedido__month="11").count()
-    dec = Pedido.objects.filter(fecha_pedido__month="12").count()
+    qs = (
+        Pedido.objects.filter(fecha_pedido__gte=inicio, fecha_pedido__lte=fin)
+        .values_list("fecha_pedido__month")
+        .annotate(num=Count("pk"))
+        .order_by("fecha_pedido__month")
+    )
 
-    # qs = Pedido.objects.filter(fecha_pedido__gte=fecha_inicio, fecha_pedido__lte=fecha_final).values("fecha_pedido",)
+    meses = [
+        "_",
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre",
+    ]
+    labels = [meses[reg[0]] for reg in qs]  # reg[0] == 1, 2, 3...
+    data = [reg[1] for reg in qs]
 
-    data = [jul, ago, sept, octu, nov, dec]
-
-    return JsonResponse(data={"pedidos": data})
+    return JsonResponse(data={"meses": labels, "num_pedidos": data})
 
 
 class ProductosMasSolicitadosView(TemplateView):
